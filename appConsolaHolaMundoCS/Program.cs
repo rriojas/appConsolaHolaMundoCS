88e09e126d72b2a693a4dbf0f1d721f75ef53b3e
Ruben Miguel Riojas Rodriguez <rriojas@hotmail.com>
2021-08-26 11:47:31 -0500

    Add project files.

---
+++ appConsolaHolaMundoCS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appConsolaHolaMundoCS
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hola Mundo!!");
+      //Sumar
+      int a = 10;
+      int b = 3;
+      int c = a / b;
+      Console.WriteLine(a + "/" + b + "=" + c);
+
+      Console.ReadKey();
+    }
+  }
+}
