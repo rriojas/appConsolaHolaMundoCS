75c84c677dfa8f742fc3c2a2f356f90e792cc92f
Ruben Miguel Riojas Rodriguez <rriojas@hotmail.com>
2021-08-26 12:00:37 -0500

    Se agrega calculo de la raiz de b

--- appConsolaHolaMundoCS/Program.cs
+++ appConsolaHolaMundoCS/Program.cs
@@ -13,9 +13,11 @@ namespace appConsolaHolaMundoCS
       Console.WriteLine("Hola Mundo!!");
       //Dividir
       int a = 10;
-      int b = 3;
+      int b = 2;
       double c = a / (b * 1.0);
       Console.WriteLine(a + "/" + b + "=" + c);
+      double r = Math.Sqrt(b);
+      Console.WriteLine("Raiz de " + b + "=" + r);
 
       Console.ReadKey();
     }
